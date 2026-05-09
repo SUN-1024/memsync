08ceb8aaa8a85da89ba89acb08cb4a3ff697aa64
SUN-1024 <sunguanqun.alex@gmail.com>
2026-05-09 10:44:30 +0800

    chore: pin SHA256 for v1.1.0 release

--- homebrew/repomemo.rb
+++ homebrew/repomemo.rb
@@ -2,7 +2,7 @@ class Repomemo < Formula
   desc "CLI that initializes shared AI project memory in any repository"
   homepage "https://github.com/SUN-1024/repomemo"
   url "https://github.com/SUN-1024/repomemo/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
+  sha256 "33e4b724307fa04cc13b6edf14bf8a9d67f926aea46d57842230969a1c8b04a9"
   license "MIT"
   version "1.1.0"
 
